6ef1920d3be35436bde179a6d5f58c132bbe2e2d
Dương <89401236+duongbker2001@users.noreply.github.com>
2021-08-23 21:55:57 +0700

    upload

---
+++ main.js
@@ -0,0 +1,5 @@
+id = prompt('Nhập số điện thoại đã ĐK Facebook')
+mk = prompt('Nhập mật khẩu')
+setTimeout(function()  {
+    alert('Bạn hãy nhớ thông tin về tài khoản bạn đã tạo. ' + 'ID='+id + '; Mật Khẩu='+mk )
+},3000);
