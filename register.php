58e0d04bd723ff3d5b1c57cd43efe12152ed88bf
Dương <89401236+duongbker2001@users.noreply.github.com>
2021-12-04 06:08:09 +0700

    Moi Nhat today

---
+++ register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+
+<form method="post" action="register.php" class="form">
+
+<h2>Đăng ký thành viên</h2>
+
+Username: <input type="text" name="username" value="" required>
+
+Password: <input type="text" name="password" value="" required/>
+
+Email: <input type="email" name="email" value="" required/>
+
+Phone: <input type="text" name="phone" value="" required/>
+
+<input type="submit" name="dangky" value="Đăng Ký"/>
+<?php require 'xuly.php';?>
+</form>
+
+</body>
+</html>
---
+++ xuly.php
@@ -0,0 +1,56 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+// Kết nối cơ sở dữ liệu
+$conn = mysqli_connect('localhost', 'root', '', 'data') or die ('Lỗi kết nối'); mysqli_set_charset($conn, "utf8");
+
+// Dùng isset để kiểm tra Form
+if(isset($_POST['dangky'])){
+$username = trim($_POST['username']);
+$password = trim($_POST['password']);
+$email = trim($_POST['email']);
+$phone = trim($_POST['phone']);
+
+
+if (empty($username)) {
+array_push($errors, "Username is required"); 
+}
+if (empty($email)) {
+array_push($errors, "Email is required"); 
+}
+if (empty($phone)) {
+array_push($errors, "Password is required"); 
+}
+if (empty($password)) {
+array_push($errors, "Two password do not match"); 
+}
+
+// Kiểm tra username hoặc email có bị trùng hay không
+$sql = "SELECT * FROM member WHERE username = '$username' OR email = '$email'";
+
+// Thực thi câu truy vấn
+$result = mysqli_query($conn, $sql);
+
+// Nếu kết quả trả về lớn hơn 1 thì nghĩa là username hoặc email đã tồn tại trong CSDL
+if (mysqli_num_rows($result) > 0)
+{
+echo '<script language="javascript">alert("Bị trùng tên hoặc chưa nhập tên!"); window.location="register.php";</script>';
+
+// Dừng chương trình
+die ();
+}
+else {
+$sql = "INSERT INTO member (username, password, email, phone) VALUES ('$username','$password','$email','$phone')";
+echo '<script language="javascript">alert("Đăng ký thành công!"); window.location="register.php";</script>';
+
+if (mysqli_query($conn, $sql)){
+echo "Tên đăng nhập: ".$_POST['username']."<br/>";
+echo "Mật khẩu: " .$_POST['password']."<br/>";
+echo "Email đăng nhập: ".$_POST['email']."<br/>";
+echo "Số điện thoại: ".$_POST['phone']."<br/>";
+}
+else {
+echo '<script language="javascript">alert("Có lỗi trong quá trình xử lý"); window.location="register.php";</script>';
+}
+}
+}
+?>
