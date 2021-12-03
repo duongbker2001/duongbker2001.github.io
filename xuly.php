79c2177b51d5225dd50a9b2a8162032628c3ec33
Dương <89401236+duongbker2001@users.noreply.github.com>
2021-12-04 06:09:06 +0700

    Moi nhat today

--- xuly.php
+++ xuly.php
@@ -1,56 +1,51 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
-// Kết nối cơ sở dữ liệu
-$conn = mysqli_connect('localhost', 'root', '', 'data') or die ('Lỗi kết nối'); mysqli_set_charset($conn, "utf8");
-
-// Dùng isset để kiểm tra Form
-if(isset($_POST['dangky'])){
-$username = trim($_POST['username']);
-$password = trim($_POST['password']);
-$email = trim($_POST['email']);
-$phone = trim($_POST['phone']);
-
-
-if (empty($username)) {
-array_push($errors, "Username is required"); 
-}
-if (empty($email)) {
-array_push($errors, "Email is required"); 
-}
-if (empty($phone)) {
-array_push($errors, "Password is required"); 
-}
-if (empty($password)) {
-array_push($errors, "Two password do not match"); 
-}
-
-// Kiểm tra username hoặc email có bị trùng hay không
-$sql = "SELECT * FROM member WHERE username = '$username' OR email = '$email'";
-
-// Thực thi câu truy vấn
-$result = mysqli_query($conn, $sql);
-
-// Nếu kết quả trả về lớn hơn 1 thì nghĩa là username hoặc email đã tồn tại trong CSDL
-if (mysqli_num_rows($result) > 0)
+//Khai báo sử dụng session
+session_start();
+//Khai báo utf-8 để hiển thị được tiếng việt
+header('Content-Type: text/html; charset=UTF-8');
+//Xử lý đăng nhập
+if (isset($_POST['dangnhap']))
 {
-echo '<script language="javascript">alert("Bị trùng tên hoặc chưa nhập tên!"); window.location="register.php";</script>';
+//Kết nối tới database
+include('connect.php');
+  
+//Lấy dữ liệu nhập vào
+$username = addslashes($_POST['username']);
+$password = addslashes($_POST['password']);
+  
+//Kiểm tra đã nhập đủ tên đăng nhập với mật khẩu chưa
+if (!$username || !$password) {
+echo "Vui lòng nhập đầy đủ tên đăng nhập và mật khẩu. <a href='javascript: history.go(-1)'>Trở lại</a>";
+exit;
+}
+  
+// mã hóa pasword
+$password = md5($password);
+  
+//Kiểm tra tên đăng nhập có tồn tại không
+$query = "SELECT username, password FROM member WHERE username='$username'";
 
-// Dừng chương trình
-die ();
-}
-else {
-$sql = "INSERT INTO member (username, password, email, phone) VALUES ('$username','$password','$email','$phone')";
-echo '<script language="javascript">alert("Đăng ký thành công!"); window.location="register.php";</script>';
+$result = mysqli_query($connect, $query) or die( mysqli_error($connect));
 
-if (mysqli_query($conn, $sql)){
-echo "Tên đăng nhập: ".$_POST['username']."<br/>";
-echo "Mật khẩu: " .$_POST['password']."<br/>";
-echo "Email đăng nhập: ".$_POST['email']."<br/>";
-echo "Số điện thoại: ".$_POST['phone']."<br/>";
+if (!$result) {
+echo "Tên đăng nhập hoặc mật khẩu không đúng!";
+} else {
+echo "Đăng nhập thành công!";
 }
-else {
-echo '<script language="javascript">alert("Có lỗi trong quá trình xử lý"); window.location="register.php";</script>';
+  
+//Lấy mật khẩu trong database ra
+$row = mysqli_fetch_array($result);
+  
+//So sánh 2 mật khẩu có trùng khớp hay không
+if ($password != $row['password']) {
+echo "Mật khẩu không đúng. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+exit;
 }
-}
+  
+//Lưu tên đăng nhập
+$_SESSION['username'] = $username;
+echo "Xin chào <b>" .$username . "</b>. Bạn đã đăng nhập thành công. <a href=''>Thoát</a>";
+die();
+$connect->close();
 }
 ?>
